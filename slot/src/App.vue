7cdbd7977e777396addc6481fe75f8e9ee7b21d7
yunkyung.lee <yunkyung.lee@navercorp.com>
2019-03-21 10:41:17 +0900

    slot add

---
+++ slot/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <twice-card :headerText="A.header" :footerText="A.footer">
+      <div>
+        <p><img :src="A.message" alt="나연 이미지"></p>
+      </div>
+    </twice-card>
+    <twice-card class="sanders" :headerText="B.header" :footerText="B.footer">
+      <div>
+        <p><img :src="B.message" alt="정연 이미지"></p>
+      </div>
+    </twice-card>
+    <twice-card class="sanders" :headerText="C.header" :footerText="C.footer">
+      <div>
+        <p><img :src="C.message" alt="모모 이미지"></p>
+      </div>
+    </twice-card>
+  </div>
+</template>
+
+<script>
+import TwiceCard from './components/TwiceCard.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TwiceCard
+  },
+  data: function() {
+    return {
+      A: {
+        header: "나연이미지",
+        footer: "출처: 네이버",
+        message: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201812%2F20181204121055605-4854115.jpg'
+      },
+      B: {
+        header: "정연이미지",
+        footer: "출처: 네이버",
+        message: "https://search.pstatic.net/common/?src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201812%2F20181204121414902-2803926.jpg"
+      },
+      C: {
+        header: "모모이미지",
+        footer: "출처: 네이버",
+        message: "https://search.pstatic.net/common/?src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201812%2F20181204121518422-6151793.jpg"
+      },
+    };
+  }
+}
+</script>
+
+<style scoped>
+.sanders { background-color: antiquewhite; }
+.sanders-content { font-family: 굴림; text-decoration: underline;}
+</style>
---
+++ slot/src/components/TwiceCard.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <div class="header">
+            {{headerText}}
+        </div>
+        <div class="content">
+            <slot></slot>
+        </div>
+        <div class="footer">
+            {{footerText}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['headerText', 'footerText']
+}
+</script>
+
+<style scoped>
+.container {
+    width:300px;
+    margin:10px;
+    padding:2px;
+    border:solid 1px gray;
+    float:left;
+}
+.header {
+    padding:4px 20px 4px 20px;
+    background-color:blue;
+    color:aqua;
+    text-align:center;
+}
+.footer {
+    padding:4px 20px 4px 20px;
+    background-color:lightgreen;
+    text-align:left;
+}
+.content {
+    padding: 10px;
+    height:auto;
+    min-height: 40px;
+    background-color:white;
+    text-align:center;
+}
+</style>
